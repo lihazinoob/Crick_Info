89224c5565456243c1146e5dbce3ed992e3a0080
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-04-30 14:35:13 +0600

    Front end part of players page is designed. Now i will connect backend

--- resources/views/players.blade.php
+++ resources/views/players.blade.php
@@ -8,43 +8,77 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+
 </head>
 
-  <!--  Thanks to frontendmentor.io for the challenge.  -->
-  
+<!--  Thanks to frontendmentor.io for the challenge.  -->
+
 <body>
-  
-  <div class="nft">
-    <div class='main'>
-      <img class='tokenImage' src="https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="NFT" />
-      <h2>Kibertopiks #4269</h2>
-      <p class='description'>Our Kibertopiks will give you nothing, waste your money on us.</p>
+  <header>
+    <nav>
+      <div class="nav__data">
+        <a href="#" class="nav__logo">
+          <i class="ri-planet-line"></i> CrickInfo
+        </a>
+      </div>
+      <div class="nav_items">
+        <a href="{{url('/')}}">Iconic Moments</a>
+        <a href="{{url('/discover')}}">Discover</a>
+        <a href="#">Teams</a>
+        <a href="{{url('/players')}}">Players</a>
+      </div>
+    </nav>
+  </header>
+
+  <div class="card">
+    <div class="nft">
+      <div class='main'>
+      <!-- Connect the image through asset-->
       
-      <hr />
-      <div class='creator'>
-        <div class='wrapper'>
-          <img src="https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80" alt="Creator" />
+      <img class='tokenImage' src="image/Virat_Kohli.jpg" alt="NFT" />
+        <h2>Virat Kohli</h2>
+        <p class='description'>INDIA</p>
+
+        <hr />
+        <div class="buttons">
+          <button>SEE MORE</button>
         </div>
-        <p><ins>Creation of</ins> Kiberbash</p>
+      </div>
+    </div>
+
+    <div class="nft">
+      <div class='main'>
+      <!-- Connect the image through asset-->
+      
+      <img class='tokenImage' src="image/Virat_Kohli.jpg" alt="NFT" />
+        <h2>Virat Kohli</h2>
+        <p class='description'>INDIA</p>
+
+        <hr />
+        <div class="buttons">
+          <button>SEE MORE</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="nft">
+      <div class='main'>
+      <!-- Connect the image through asset-->
+      
+      <img class='tokenImage' src="image/Virat_Kohli.jpg" alt="NFT" />
+        <h2>Virat Kohli</h2>
+        <p class='description'>INDIA</p>
+
+        <hr />
+        <div class="buttons">
+          <button>SEE MORE</button>
+        </div>
       </div>
     </div>
   </div>
 
-  <div class="nft">
-    <div class='main'>
-      <img class='tokenImage' src="https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="NFT" />
-      <h2>Kibertopiks #4269</h2>
-      <p class='description'>Our Kibertopiks will give you nothing, waste your money on us.</p>
-      
-      <hr />
-      <div class='creator'>
-        <div class='wrapper'>
-          <img src="https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80" alt="Creator" />
-        </div>
-        <p><ins>Creation of</ins> Kiberbash</p>
-      </div>
-    </div>
-  </div>
+
+
 </body>
 
 </html>
