683ed9df386361c4a9c6d7cc0a0dc88d419fc4e1
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-05-10 22:19:01 +0600

    ParticularPlayer Web Page is Under Design

--- resources/views/particularIntlTeam.blade.php
+++ resources/views/particularIntlTeam.blade.php
@@ -2,12 +2,35 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
+  <!-- Link the css file with asset -->
+  <link rel="stylesheet" href="{{ asset('cssfiles/particularIntlTean.css') }}">
   <title>{{$team->teamname}}</title>
 </head>
+
 <body>
-  <h1>{{$team->teamname}}</h1>
+  <header class="header">
+    <a href="#home" class="logo"><span>Teams</span> </a>
+    <i class="fa-solid fa-bars" id="menu-icon"></i>
+
+    <nav class="navbar">
+      <a href="#men" class="active"> Men's International </a>
+      <a href="#women"> Women's International </a>
+      <a href="#services"> IPL </a>
+      <a href="#portfolio"> BPL </a>
+      <a href="#contact"> BBL </a>
+    </nav>
+  </header>
+
+  <section class="cover-image">
+    <img src="{{asset('teamcoverimages/'.$team->teamcoverimage)}}" alt="">
+  </section>
+
+
 </body>
+
 </html>
