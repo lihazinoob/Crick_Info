5ede3ad54d31ee9a3dbfd6ea52f5a8e47b765ee5
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-05-15 15:56:04 +0600

    Side bar and nav bar of adin panel is almost designed

--- resources/views/adminpanel.blade.php
+++ resources/views/adminpanel.blade.php
@@ -79,7 +79,6 @@
     <!-- NAVBAR -->
     <nav>
       <i class='bx bx-menu'></i>
-      <a href="#" class="nav-link">Categories</a>
       <form action="#">
         <div class="form-input">
           <input type="search" placeholder="Search...">
@@ -88,13 +87,7 @@
       </form>
       <input type="checkbox" id="switch-mode" hidden>
       <label for="switch-mode" class="switch-mode"></label>
-      <a href="#" class="notification">
-        <i class='bx bxs-bell'></i>
-        <span class="num">8</span>
-      </a>
-      <a href="#" class="profile">
-        <img src="img/people.png">
-      </a>
+      
     </nav>
     <!-- NAVBAR -->
 
