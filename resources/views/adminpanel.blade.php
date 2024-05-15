dbf485987023ee2a3e02546234f31db7ad6a9c28
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-05-15 16:23:48 +0600

    Create iconic moments form in integrated with Admin panel Dashboard

--- resources/views/adminpanel.blade.php
+++ resources/views/adminpanel.blade.php
@@ -61,7 +61,7 @@
       </li>
     </ul>
     <ul class="side-menu">
-      
+
       <li>
         <a href="#" class="logout">
           <i class='bx bxs-log-out-circle'></i>
@@ -87,145 +87,66 @@
       </form>
       <input type="checkbox" id="switch-mode" hidden>
       <label for="switch-mode" class="switch-mode"></label>
-      
+
     </nav>
     <!-- NAVBAR -->
 
     <!-- MAIN -->
     <main>
-      <div class="head-title">
-        <div class="left">
-          <h1>Dashboard</h1>
-          <ul class="breadcrumb">
-            <li>
-              <a href="#">Dashboard</a>
-            </li>
-            <li><i class='bx bx-chevron-right'></i></li>
-            <li>
-              <a class="active" href="#">Home</a>
-            </li>
-          </ul>
-        </div>
-        <a href="#" class="btn-download">
-          <i class='bx bxs-cloud-download'></i>
-          <span class="text">Download PDF</span>
-        </a>
-      </div>
+      <!-- Form of creating iconic moments -->
 
-      <ul class="box-info">
-        <li>
-          <i class='bx bxs-calendar-check'></i>
-          <span class="text">
-            <h3>1020</h3>
-            <p>New Order</p>
-          </span>
-        </li>
-        <li>
-          <i class='bx bxs-group'></i>
-          <span class="text">
-            <h3>2834</h3>
-            <p>Visitors</p>
-          </span>
-        </li>
-        <li>
-          <i class='bx bxs-dollar-circle'></i>
-          <span class="text">
-            <h3>$2543</h3>
-            <p>Total Sales</p>
-          </span>
-        </li>
-      </ul>
-
-
-      <div class="table-data">
-        <div class="order">
-          <div class="head">
-            <h3>Recent Orders</h3>
-            <i class='bx bx-search'></i>
-            <i class='bx bx-filter'></i>
+      <div class="icon">
+        <form action="/store" method="POST" enctype="multipart/form-data">
+          @csrf
+          <div class="container">
+            <h1>Submit Your Image</h1>
+            <form action="submit.php" method="post" enctype="multipart/form-data">
+              <div class="form-group">
+                <label for="image">Image:</label>
+                <input type="file" id="image" name="image" accept="image/*" required value="{{old('image')}}">
+                @if($errors->has('image'))
+                <p>{{ $errors->first('image') }}</p>
+                @endif
+              </div>
+              <div class="form-group">
+                <label for="author">Author:</label>
+                <input type="text" id="author" name="author" required value="{{old('description')}}">
+                @if ($errors->has('author'))
+                <p>{{ $errors->first('author') }}</p>
+                @endif
+              </div>
+              <div class="form-group">
+                <label for="title">Title:</label>
+                <input type="text" id="title" name="title" required value="{{old("title")}}">
+                @if ($errors->has('title'))
+                <p>{{ $errors->first('title') }}</p>
+                @endif
+              </div>
+              <div class="form-group">
+                <label for="topic">Topic:</label>
+                <input type="text" id="topic" name="topic" required value="{{old("topic")}}">
+                @if ($errors->has('topic'))
+                <p>{{ $errors->first('topic') }}</p>
+                @endif
+              </div>
+              <div class="form-group">
+                <label for="description">Description:</label>
+                <textarea id="description" name="description" rows="5" required value="{{old("description")}}"></textarea>
+                @if ($errors->has('description'))
+                <p>{{ $errors->first('description') }}</p>
+                @endif
+              </div>
+              <button type="submit">Submit</button>
+            </form>
           </div>
-          <table>
-            <thead>
-              <tr>
-                <th>User</th>
-                <th>Date Order</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <img src="img/people.png">
-                  <p>John Doe</p>
-                </td>
-                <td>01-10-2021</td>
-                <td><span class="status completed">Completed</span></td>
-              </tr>
-              <tr>
-                <td>
-                  <img src="img/people.png">
-                  <p>John Doe</p>
-                </td>
-                <td>01-10-2021</td>
-                <td><span class="status pending">Pending</span></td>
-              </tr>
-              <tr>
-                <td>
-                  <img src="img/people.png">
-                  <p>John Doe</p>
-                </td>
-                <td>01-10-2021</td>
-                <td><span class="status process">Process</span></td>
-              </tr>
-              <tr>
-                <td>
-                  <img src="img/people.png">
-                  <p>John Doe</p>
-                </td>
-                <td>01-10-2021</td>
-                <td><span class="status pending">Pending</span></td>
-              </tr>
-              <tr>
-                <td>
-                  <img src="img/people.png">
-                  <p>John Doe</p>
-                </td>
-                <td>01-10-2021</td>
-                <td><span class="status completed">Completed</span></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="todo">
-          <div class="head">
-            <h3>Todos</h3>
-            <i class='bx bx-plus'></i>
-            <i class='bx bx-filter'></i>
-          </div>
-          <ul class="todo-list">
-            <li class="completed">
-              <p>Todo List</p>
-              <i class='bx bx-dots-vertical-rounded'></i>
-            </li>
-            <li class="completed">
-              <p>Todo List</p>
-              <i class='bx bx-dots-vertical-rounded'></i>
-            </li>
-            <li class="not-completed">
-              <p>Todo List</p>
-              <i class='bx bx-dots-vertical-rounded'></i>
-            </li>
-            <li class="completed">
-              <p>Todo List</p>
-              <i class='bx bx-dots-vertical-rounded'></i>
-            </li>
-            <li class="not-completed">
-              <p>Todo List</p>
-              <i class='bx bx-dots-vertical-rounded'></i>
-            </li>
-          </ul>
-        </div>
+        </form>
       </div>
+      
+
+
+
+
+
     </main>
     <!-- MAIN -->
   </section>
