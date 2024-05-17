c7eab64c61566948d273bf14dbb79267fe691614
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-05-18 00:33:24 +0600

    Team List WebPage design is ready

--- resources/views/TeamList.blade.php
+++ resources/views/TeamList.blade.php
@@ -6,6 +6,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
   <link rel="stylesheet" href="{{ asset('cssfiles/TeamList.css') }}">
   <title>Team List</title>
@@ -18,17 +19,18 @@
     <i class="fa-solid fa-bars" id="menu-icon"></i>
 
     <nav class="navbar">
-      <a href="#men" class="active"> Men's International </a>
-      <a href="#women"> Women's International </a>
+      <a href="#men" class="active"> International </a>
       <a href="#services"> IPL </a>
       <a href="#portfolio"> BPL </a>
       <a href="#contact"> BBL </a>
+      <a href=""><i class='bx bx-log-out'></i>LOGOUT</a>
+
     </nav>
   </header>
 
   <section class="men" id="men">
 
-    <h2>Men's International</h2>
+    <h2>International</h2>
     <div class="items">
       @foreach($teams as $team)
       <div class="profile-card">
@@ -56,9 +58,62 @@
 
   </section>
 
-  <section class="women" id="women">
+  <section class="ipl" id="ipl">
+    <h2>IPL Teams</h2>
+    <div class="items">
+      @foreach($teams as $team)
+      <div class="profile-card">
+        <div class="image">
+          <!-- Link an image with asset -->
+          <img src="{{ asset('teamicons/'.$team->teamicon) }}" alt="" class="profile-img">
 
-  </section>
+          <!-- <img src="image/logo_discover" alt="" class="profile-img"> -->
+        </div>
+        <div class="text-data">
+          <span class="name">
+            {{$team->teamname}}
+          </span>
+
+
+          <div class="buttons">
+            <button><a href="/particularIntlteam/{{$team->id}}">
+                SEE MORE</a></button>
+          </div>
+        </div>
+      </div>
+      @endforeach
+
+    </div>
+    </section>
+    <section class="bpl" id="bpl">
+    <h2>BPL Teams</h2>
+    <div class="items">
+      @foreach($teams as $team)
+      <div class="profile-card">
+        <div class="image">
+          <!-- Link an image with asset -->
+          <img src="{{ asset('teamicons/'.$team->teamicon) }}" alt="" class="profile-img">
+
+          <!-- <img src="image/logo_discover" alt="" class="profile-img"> -->
+        </div>
+        <div class="text-data">
+          <span class="name">
+            {{$team->teamname}}
+          </span>
+
+
+          <div class="buttons">
+            <button><a href="/particularIntlteam/{{$team->id}}">
+                SEE MORE</a></button>
+          </div>
+        </div>
+      </div>
+      @endforeach
+
+    </div>
+    </section>
+
+  
 
 
 </body>
