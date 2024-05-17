0d495c9817ead04775b5b29c5e68e982e7869343
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-05-17 22:43:16 +0600

    Login and registration page is fully completed

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-  <h2>USER LOGIN</h2>
+  <h2>USER LOGIN & REGISTRATION</h2>
   <div class="container" id="container">
     <div class="form-container sign-up-container">
       <form action="/register" method="POST">
@@ -41,11 +41,11 @@
       <div class="overlay">
         <div class="overlay-panel overlay-left">
           <h1>Welcome Back!</h1>
-          <p>To keep connected with us please login with your personal info</p>
+          <p>LOGIN and EXPLORE a New WORLD of CRICKET</p>
           <button class="ghost" id="signIn">Sign In</button>
         </div>
         <div class="overlay-panel overlay-right">
-          <h1>Hello, Friend!</h1>
+          <h1>Hello, CRICKLOVER!</h1>
           <p>Enter your personal details and start journey with us</p>
           <button class="ghost" id="signUp">Sign Up</button>
         </div>
@@ -53,14 +53,7 @@
     </div>
   </div>
 
-  <footer>
-    <p>
-      Created with <i class="fa fa-heart"></i> by
-      <a target="_blank" href="https://florin-pop.com">Florin Pop</a>
-      - Read how I created this and how you can join the challenge
-      <a target="_blank" href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/">here</a>.
-    </p>
-  </footer>
+  
   <script src="{{ asset('jsfiles/login.js') }}"></script>
 </body>
 
