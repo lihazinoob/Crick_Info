77e76546e422bb41ab1ddc22901f41579616a6c0
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-05-17 13:08:18 +0600

    Form toggle work is on

--- resources/views/adminpanel.blade.php
+++ resources/views/adminpanel.blade.php
@@ -95,7 +95,7 @@
     <main>
       <!-- Form of creating iconic moments -->
 
-      <div class="icon">
+      <div class="icon form-container" id="iconinc-memont-form">
         <form action="/store" method="POST" enctype="multipart/form-data">
           @csrf
           <div class="container">
@@ -141,7 +141,71 @@
           </div>
         </form>
       </div>
-      
+      <!-- Form of creating players -->
+
+      <div class="player form-container" id="player-create-form">
+        <h1>Register Player</h1>
+        <div class="container">
+          <form action="/listofplayers" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+              <label for="player-image">Player Image:</label>
+              <input type="file" id="image" name="image" accept="image/*" required value="{{old('image')}}">
+              @if ($errors->has('image'))
+              <p>{{ $errors->first('image') }}</p>
+              @endif
+            </div>
+            <div class="form-group">
+              <label for="full-name">Full Name:</label>
+              <input type="text" id="full-name" name="full-name" required value="{{old('full-name')}}">
+              @if ($errors->has('full-name'))
+              <p>{{ $errors->first('full-name') }}</p>
+              @endif
+            </div>
+            <div class="form-group">
+              <label for="birth-date">Birth Date:</label>
+              <input type="date" id="birth-date" name="birth-date" required value="{{old('birth-date')}}">
+              @if ($errors->has('birth-date'))
+              <p>{{ $errors->first('birth-date') }}</p>
+              @endif
+            </div>
+            <div class="form-group">
+              <label for="batting-style">Batting Style:</label>
+              <input type="text" id="batting-style" name="batting-style" placeholder="e.g., Right-handed, Left-handed" value="{{old('batting-style')}}">
+            </div>
+            <div class="form-group">
+              <label for="bowling-style">Bowling Style:</label>
+              <input type="text" id="bowling-style" name="bowling-style" placeholder="e.g., Right-arm, Left-arm" value="{{old('bowling-style')}}">
+            </div>
+            <div class="form-group">
+              <label for="playing-role">Playing Role:</label>
+              <input type="text" id="playing-role" name="playing-role" required value="{{old('playing-role')}}">
+              @if ($errors->has('playing-role'))
+              <p>{{ $errors->first('playing-role') }}</p>
+              @endif
+            </div>
+            <div class="form-group">
+              <label for="country">Country:</label>
+              <input type="text" id="country" name="country" required value="{{old('country')}}">
+              @if ($errors->has('country'))
+              <p>{{ $errors->first('country') }}</p>
+              @endif
+            </div>
+            <div class="form-group">
+              <label for="international_team">International Team:</label>
+              <input type="text" id="international_team" name="international_team" required value="{{old('international_team')}}">
+              @if ($errors->has('international_team'))
+              <p>{{ $errors->first('international_team') }}</p>
+              @endif
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+
+      </div>
+
+
+
 
 
 
