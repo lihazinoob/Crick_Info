bee9c90432651669abfd8a8a27aa581b48b812f4
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-05-19 02:13:29 +0600

    Particular stadium is designed

--- resources/views/particularstadium.blade.php
+++ resources/views/particularstadium.blade.php
@@ -17,8 +17,9 @@
 
     <nav class="navbar">
       <a href="#about"> Know US</a>
-      <a href="#players"> Players </a>
+
       <a href="#history"> Historical Events </a>
+      <a href="#feature"> Features</a>
 
 
 
@@ -38,6 +39,68 @@
         <p>
           {{$stadium->stadiumdescription}}
         </div>
+    </div>
+  </section>
+
+  <section class="about" id="#history">
+    <h1>TOP MOMENTS</h1>
+    <div class="about-infoholder">
+      <div class="image-holder">
+        <img src="{{asset('stadium/'.$stadium->topmomentpicture1)}}" alt="">
+      </div>
+      <div class="text-holder">
+        <p>
+          {{$stadium->topmomenttext1}}
+        </div>
+    </div>
+  </section>
+  <section class="about" id="">
+    <h1>TOP MOMENTS</h1>
+    <div class="about-infoholder">
+      <div class="image-holder">
+        <img src="{{asset('stadium/'.$stadium->topmomentpicture2)}}" alt="">
+      </div>
+      <div class="text-holder">
+        <p>
+          {{$stadium->topmomenttext2}}
+        </div>
+    </div>
+  </section>
+  <section class="about" id="">
+    <h1>TOP MOMENTS</h1>
+    <div class="about-infoholder">
+      <div class="image-holder">
+        <img src="{{asset('stadium/'.$stadium->topmomentpicture3)}}" alt="">
+      </div>
+      <div class="text-holder">
+        <p>
+          {{$stadium->topmomenttext3}}
+        </div>
+    </div>
+  </section>
+  <section class="about" id="">
+    <h1>TOP MOMENTS</h1>
+    <div class="about-infoholder">
+      <div class="image-holder">
+        <img src="{{asset('stadium/'.$stadium->topmomentpicture4)}}" alt="">
+      </div>
+      <div class="text-holder">
+        <p>
+          {{$stadium->topmomenttext4}}
+        </div>
+    </div>
+  </section>
+
+  <section class="feature" id="feature">
+    <h1>Features</h1>
+    <div class="feature-card">
+      <div class="featureimage">
+        <img src="{{asset('stadium/'.$stadium->featurepicture)}}" alt="">
+      </div>
+      <div class="featuretext">
+        
+        <p>{{$stadium->featuredescription}}</p>
+      </div>
     </div>
   </section>
 
